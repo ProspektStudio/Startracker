250ae637f33e78f6b63d2091c3fcdeb2b2a59be2
David Thomason <dtom90@users.noreply.github.com>
2025-04-21 12:20:20 -0500

    get satellite info from gemini

--- api/main.py
+++ api/main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI, Query
 from dotenv import load_dotenv
 import os
-from typing import Optional
+import requests
+from google import genai
+from google.genai import types
 
 app = FastAPI()
 
-# load the api key from the .env file
+# set up the gemini client
 load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
-print(api_key)
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_MODEL = "gemini-2.0-flash"
+gemini_client = genai.Client(api_key=GEMINI_API_KEY)
 
 @app.get("/")
 async def read_root():
@@ -19,10 +22,8 @@ async def get_satellite_info(
     group: str = Query(..., min_length=1, max_length=50),
     name: str = Query(..., min_length=1, max_length=50)
 ):
-    # Parameters are automatically validated
-    # No need for manual sanitization
+    response = gemini_client.models.generate_content(model=GEMINI_MODEL, contents=f"Give me information about the satellite {name} in the group {group}")
     return {
-        "group": group,
-        "name": name
-    } 
+        "satellite_info": response.text
+    }
 
